76712cce15d62de417788f8944d27a40dbb8060a
Bartek Walkowicz <wallko1@hotmail.com>
2018-03-21 09:23:41 +0100

    mpi ex3 change types used to long long and long double

--- mpi_3/main.c
+++ mpi_3/main.c
@@ -5,13 +5,12 @@
 
 
 #define RAND ((long double)rand() / (long double)RAND_MAX)
-#define ITERATIONS 1000000
 
 
-int pi(int iterations){
-    int in_circle = 0;
+long long monte_carlo(long long iterations){
+    long long in_circle = 0;
     long double x, y;
-    for(int i = 0; i < iterations; ++i){
+    for(long i = 0; i < iterations; ++i){
         x = RAND;
         y = RAND;
         if((x*x + y*y) <= 1)
@@ -21,29 +20,33 @@ int pi(int iterations){
 }
 
 
-int main(){
+int main(int argc, char *argv[]){
     int rank, size;
     long double t1, t2;
+
+    int exponent = atoi(argv[1]);
+    long long iterations = 10;
+    for(int i = 1; i < exponent; ++i)
+        iterations *= 10;
 
     MPI_Init(NULL, NULL);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     MPI_Comm_size(MPI_COMM_WORLD, &size);
+
     srand(rank);
+    long long in_circle_global;
+    long long points_overall = size*iterations;
 
     MPI_Barrier(MPI_COMM_WORLD);
+
     t1 = MPI_Wtime();
-
-    int in_circle = pi(ITERATIONS);
-    int all_in_circle = 0;
-    int all_points = size*ITERATIONS;
-
-    MPI_Reduce(&in_circle, &all_in_circle, 1, MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
+    long long in_circle_local = monte_carlo(iterations);
+    MPI_Reduce(&in_circle_local, &in_circle_global, 1, MPI_LONG_LONG_INT, MPI_SUM, 0, MPI_COMM_WORLD);
+    long double pi = 4.0*(long double)in_circle_global/(long double)points_overall;
     t2 = MPI_Wtime();
     
-    if(rank == 0){
-        long double po_2_pd = ((long double)all_in_circle)/((long double)all_points);
-        printf("\nTIME: %.10Lf\nPI: %.10Lf\n", t2-t1, 4.0*po_2_pd);
-    }
+    if(rank == 0)
+        printf("%.10Lf %.10Lf\n", t2-t1, pi);
 
     MPI_Finalize();
     return 0;
